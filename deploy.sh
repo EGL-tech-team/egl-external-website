6ab38a8cc3ffa14acdc1388db7dc1d5b695624ab
Steven Pecht <steven.pecht@xconcepts.net>
2014-04-17 12:51:33 -0400

    Changed prompt from username to uniquename for clarity.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Batch script to deploy the website to a given users IFS space for testing
-echo "Enter username, follow by [ENTER]:"
+echo "Enter uniquename, follow by [ENTER]:"
 read username
 
 echo "Enter folder to delpoy to, followed by [ENTER]:"
